3d4d7e5dcce70363a45506152520de34364f0124
GDalforno <gabriel.dalforno@semantix.ai>
2024-04-15 10:54:23 -0300

    feat: add semantix agent tool input

---
+++ tests/test_semantix_agent_tool_input.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+import json
+
+from pydantic import BaseModel
+
+
+class SemantixAgentToolInput(BaseModel):
+    @classmethod
+    def query_to_tool_input(cls, query: str) -> SemantixAgentToolInput:
+        return cls.model_validate_json(query)
+
+
+class TestSemantixAgentToolInput:
+    def test_its_children_class_should_be_able_to_parse_json_string_to_its_instance(self) -> None:
+
+        class SemantixAgentToolInputChild(SemantixAgentToolInput):
+            input_1: str
+            input_2: Input2
+
+            class Input2(SemantixAgentToolInput):
+                input_2_1: int
+
+
+        sut = SemantixAgentToolInputChild
+
+        semantix_agent_tool_input = sut.query_to_tool_input(json.dumps({"input_1": "x", "input_2": {"input_2_1": 0}}))
+
+        assert semantix_agent_tool_input.input_1 == "x"
+        assert semantix_agent_tool_input.input_2.input_2_1 == 0
