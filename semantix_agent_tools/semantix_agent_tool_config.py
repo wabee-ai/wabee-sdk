9fe9d3b3afd5ffddd2f4de46014f9be4cdbcc258
GDalforno <gabriel.dalforno@semantix.ai>
2024-04-16 13:51:47 -0300

    test: ensure llm type is checked when initializing semantix agent tool config

--- semantix_agent_tools/semantix_agent_tool_config.py
+++ semantix_agent_tools/semantix_agent_tool_config.py
@@ -1,10 +1,22 @@
-# from langchain.schema.language_model import BaseLanguageModel
 from typing import Any
 
+from langchain.schema.language_model import BaseLanguageModel
 from pydantic import BaseModel
+
+from semantix_agent_tools.semantix_agent_tool_field_validator import (
+    semantix_agent_tool_field_validator,
+)
 
 
 class SemantixAgentToolConfig(BaseModel):
     name: str
     description: str
     llm: Any
+
+    @semantix_agent_tool_field_validator("llm")
+    def check_llm(cls, value: Any) -> Any:
+        if not isinstance(value, BaseLanguageModel):
+            raise ValueError(
+                "Provided llm does not implement the BaseLanguageModel interface"
+            )
+        return value
--- tests/test_semantix_agent_tool_config.py
+++ tests/test_semantix_agent_tool_config.py
@@ -1,3 +1,4 @@
+import pytest
 from langchain_community.llms.fake import FakeListLLM
 
 from semantix_agent_tools.semantix_agent_tool_config import SemantixAgentToolConfig
@@ -15,3 +16,11 @@ class TestSemantixAgentToolConfig:
         assert sut.name == "any_name"
         assert sut.description == "any_description"
         assert sut.llm == fake_llm
+
+    def test_should_check_if_llm_belongs_to_allowed_llms_by_langchain(self) -> None:
+        class InvalidLLM: ...
+
+        with pytest.raises(ValueError):
+            SemantixAgentToolConfig(
+                name="any_name", description="any_description", llm=InvalidLLM()
+            )
