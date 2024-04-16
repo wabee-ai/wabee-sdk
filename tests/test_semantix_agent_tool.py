f88d3d57ec37e84f99a5327c45e56017f368270c
GDalforno <gabriel.dalforno@semantix.ai>
2024-04-16 15:11:22 -0300

    fix: remove duplicated line of code from semantix agent tool test suite

--- tests/test_semantix_agent_tool.py
+++ tests/test_semantix_agent_tool.py
@@ -15,7 +15,6 @@ class TestSemantixAgentTool:
 
         assert sut.name == "any_name"
         assert sut.description == "any_description"
-        assert sut.description == "any_description"
 
     def test_ensure_children_of_semantix_agent_tool_cannot_be_created_without_implement_the_parent_interface(
         self,
